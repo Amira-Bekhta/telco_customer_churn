91e94932c8ceec9c03862889a3cbdd780e2d4ca1
Amira Bekhta <amira25bkh@gmail.com>
2025-10-29 13:58:19 +0100

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,14 +1,13 @@
-print("lessgo")
 import joblib
 from data_loader import load_data
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
 
-print("Imports done")
+
 
 data = load_data("model_data.csv", "preprocessed", True)
 
-print("loaded model")
+
 
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
@@ -19,10 +18,9 @@ model = GradientBoostingClassifier()
 
 model.fit(X_train, y_train)
 
-print("fitting done")
+
 
 joblib.dump(model, "model/model.pkl")
 joblib.dump(X_test, "model/X_test.pkl")
 joblib.dump(y_test, "model/y_test.pkl")
 
-print("Everything done for you, boss!")
